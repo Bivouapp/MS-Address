262679ec316276da600b0bb8b1b360c88b6ad279
NathBel <bellus.nathan@gmail.com>
2024-11-22 14:44:04 +0100

    :sparkles: feat: Add create and getAll endpoints

--- src/main/java/com/example/MSAddress/controllers/AddressController.java
+++ src/main/java/com/example/MSAddress/controllers/AddressController.java
@@ -1,13 +1,15 @@
 package com.example.MSAddress.controllers;
 
+import com.example.MSAddress.models.Address;
+import com.example.MSAddress.services.AddressService;
 import com.example.MSAddress.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/addresses")
@@ -17,14 +19,24 @@ public class AddressController {
     private com.example.MSAddress.repositories.AddressRepository addressRepository;
 
     @Autowired
-    private com.example.MSAddress.services.AddressService addressService;
+    private AddressService addressService;
 
     @Autowired
     private JwtUtils jwtUtils;
 
+    @PostMapping
+    public ResponseEntity<Address> createAddress(@RequestBody Address address) {
+        try {
+            Address createdAddress = addressService.saveAddress(address);
+            return new ResponseEntity<>(createdAddress, HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Unable to create address", e);
+        }
+    }
 
-
-
-
-
+    @GetMapping
+    public ResponseEntity<List<Address>> getAllAddresses() {
+        List<Address> addresses = addressService.getAllAddresses();
+        return new ResponseEntity<>(addresses, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/MSAddress/services/AddressService.java
+++ src/main/java/com/example/MSAddress/services/AddressService.java
@@ -1,11 +1,36 @@
 package com.example.MSAddress.services;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import com.example.MSAddress.models.Address;
+import com.example.MSAddress.repositories.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class AddressService {
 
+    @Autowired
+    private AddressRepository addressRepository;
+
+
+    public Address saveAddress(Address address) {
+        return addressRepository.save(address);
+    }
+
+
+    public List<Address> getAllAddresses() {
+        return addressRepository.findAll();
+    }
+
+
+    public Optional<Address> getAddressById(Long id) {
+        return addressRepository.findById(id);
+    }
+
+
+    public void deleteAddress(Long id) {
+        addressRepository.deleteById(id);
+    }
 }
